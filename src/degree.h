11e9b9d7359ecbe83a5995882be2bb491888c617
Hannah Rose <misshannahbergman@gmail.com>
2023-02-17 14:08:35 -0500

    Create degree.h

---
+++ src/degree.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+enum DegreeProgram {UNDECIDED, SECURITY, NETWORK, SOFTWARE};
+
+static const string degreeProgramString[] = {
+    "UNDECIDED",
+    "SECURITY",
+    "NETWORK",
+    "SOFTWARE"
+};
